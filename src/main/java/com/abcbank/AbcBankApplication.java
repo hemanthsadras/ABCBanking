9529a0291a52eeecd583b694ec20a1a3ea4a4c54
hemanthsadras <hemanthsadras@gmail.com>
2018-02-15 11:56:42 +0530

    inital commit

---
+++ src/main/java/com/abcbank/AbcBankApplication.java
@@ -0,0 +1,12 @@
+package com.abcbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcBankApplication.class, args);
+	}
+}
